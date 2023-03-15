93ee35ed32593a71a06f1aa5e6e9b8407e7feb1a
foreverzmy <921255465@qq.com>
2023-03-15 19:41:04 +0800

    fix: set version to global

--- cmd/version.go
+++ cmd/version.go
@@ -6,13 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "v0.0.1"
+var Version = "v0.0.1"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of AISH",
 	Long:  `All software has versions. This is AISH's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
+		fmt.Println(Version)
 	},
 }
